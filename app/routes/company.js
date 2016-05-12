23d62f2b15e679e0f20adaa1d5126862a71028be
charlie <cswanso2@illinois.edu>
2016-05-11 20:05:13 -0400

    working chart

--- app/routes/company.js
+++ app/routes/company.js
@@ -3,57 +3,21 @@ import Ember from 'ember';
 var companies = [
   {
     id: 1,
-    name: 'China',
-    symbol: 'Asia',
-    prices: [3,4,5,3,2,1]/*[{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},
+    name: 'Google',
+    symbol: 'Goog',
+    prices: 
+    {
+      labels: ["4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","4/11/2016","5/02/2016",
+      "5/03/2016","5/04/2016","5/05/2016","5/06/2016","5/07/2016","5/09/2016","5/10/2016","5/11/2016"],
+      datasets:[{ 
+        data:[4.41,4.40,5.31,3.31,2.25,1.43,4.41,4.40,5.31,3.31,2.25,1.43,4.41,4.40,5.31,3.31,2.25,1.43,4.41,4.40,5.31,3.31,2.25,1.43,4.41,4.40,5.31,3.31,2.25,1.43],
+        label: "date"
+      }]
+      /*[{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},
     {dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},
     {dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1},{dollars: 5, cents: 1}]*/
+    }
   },
-  {
-    id: 2,
-    name: 'India',
-    symbol: 'Asia',
-  },
-  {
-    id: 3,
-    name: 'USA',
-    symbol: 'America',
-  },
-  {
-    id: 4,
-    name: 'Indonesia',
-    symbol: 'Asia',
-  },
-  {
-    id: 5,
-    name: 'Brazil',
-    symbol: 'America',
-  },
-  {
-    id: 6,
-    name: 'Pakistan',
-    symbol: 'Asia',
-  },
-  {
-    id: 7,
-    name: 'Nigeria',
-    symbol: 'Africa',
-  },
-  {
-    id: 8,
-    name: 'Bangladesh',
-    symbol: 'Asia',
-  },
-  {
-    id: 9,
-    name: 'Russia',
-    symbol: 'Europe',
-  },
-  {
-    id: 10,
-    name: 'Japan',
-    symbol: 'Asia',
-  }
 ];
 
 export default Ember.Route.extend({
